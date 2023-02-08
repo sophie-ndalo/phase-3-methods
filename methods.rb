0ed2a5594edb8a6175bdbee853dd106f89a325e5
sophia <sophiendalo@gmail.com>
2023-02-08 18:42:13 +0300

    completed assignment

--- methods.rb
+++ methods.rb
@@ -1,1 +1,26 @@
 # Your code here!
+def greet_programmer
+    puts "Hello, programmer!"
+  end
+  
+  def greet(name)
+    puts "Hello, #{name}!"
+  end
+
+  def greet_with_default(name = "Naureen")
+    puts "Hello, #{name}!"
+  end
+
+  def greet_with_default(name = "programmer")
+    puts "Hello, #{name}!"
+  end
+  
+  def add(a, b)
+    a + b
+  end
+  
+  def halve(n)
+    n.is_a?(Integer) ? n / 2 : nil
+  end
+  
+  
